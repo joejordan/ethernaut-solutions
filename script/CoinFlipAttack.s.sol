b2a61465999f9e19bbe8302d9582ecf169569e21
Joe Jordan <dev@ignitesoft.com>
2022-08-15 23:53:32 -0400

    script experiments

--- script/CoinFlipAttack.s.sol
+++ script/CoinFlipAttack.s.sol
@@ -5,12 +5,17 @@ import { Script } from "forge-std/Script.sol";
 import { CoinFlipAttack } from "../src/CoinFlip/CoinFlipAttack.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract CoinFlipScript is Script {
+contract CoinFlipAttackScript is Script {
     CoinFlipAttack internal coinFlipAttacker;
 
     function run() public {
         vm.startBroadcast();
         coinFlipAttacker = new CoinFlipAttack();
+        callCoinFlip(address(0x677F2b9b7b5CAdC57dc27880004cb914494BA0F7));
         vm.stopBroadcast();
+    }
+
+    function callCoinFlip(address coinFlipInstance) public {
+        coinFlipAttacker.callCoinFlip(coinFlipInstance);
     }
 }
