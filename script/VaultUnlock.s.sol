faab25e4231327b93bc69302a648efcc9ea2d79e
Joe Jordan <dev@ignitesoft.com>
2022-08-18 00:30:25 -0400

    Vault setup

---
+++ script/VaultUnlock.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import { Script } from "forge-std/script.sol";
+import { console } from "forge-std/console.sol";
+
+contract VaultUnlock is Script {
+    function run() public {
+        vm.startBroadcast();
+        // todo
+        vm.stopBroadcast();
+    }
+}
