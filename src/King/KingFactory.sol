3f1744e15dc5dc77a7de73b670986b02fd543287
Joe Jordan <dev@ignitesoft.com>
2022-08-21 22:47:28 -0400

    add KingFactory Ethernaut contract

---
+++ src/King/KingFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+// https://github.com/OpenZeppelin/ethernaut/blob/master/contracts/contracts/levels/KingFactory.sol
+
+pragma solidity ^0.8.15;
+
+import "../Level.sol";
+import "./King.sol";
+
+contract KingFactory is Level {
+    uint256 public insertCoin = 0.001 ether;
+
+    function createInstance(address _player) public payable override returns (address) {
+        _player;
+        require(msg.value >= insertCoin, "Must send at least 0.001 ETH");
+        return address((new King){ value: msg.value }());
+    }
+
+    function validateInstance(address payable _instance, address _player) public override returns (bool) {
+        _player;
+        King instance = King(_instance);
+        (bool result, ) = address(instance).call{ value: 0 }("");
+        !result;
+        return instance._king() != address(this);
+    }
+
+    receive() external payable {}
+}
