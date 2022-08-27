22ae15533b65339fea6af64142eefb8e9bee5eab
Joe Jordan <dev@ignitesoft.com>
2022-08-27 16:02:57 -0400

    additional revert comments to target contract

--- src/GatekeeperOne/GatekeeperOne.sol
+++ src/GatekeeperOne/GatekeeperOne.sol
@@ -2,22 +2,27 @@
 pragma solidity ^0.8.15;
 
 import "@openzeppelin/utils/math/SafeMath.sol";
+import { console } from "forge-std/console.sol";
 
 contract GatekeeperOne {
     using SafeMath for uint256;
     address public entrant;
 
     modifier gateOne() {
-        require(msg.sender != tx.origin);
+        require(msg.sender != tx.origin, "FAILED GATE ONE");
         _;
     }
 
     modifier gateTwo() {
-        require(gasleft().mod(8191) == 0);
+        console.log(gasleft());
+        require(gasleft().mod(8191) == 0, "FAILED GATE TWO");
         _;
     }
 
     modifier gateThree(bytes8 _gateKey) {
+        console.logString("PART ONEEEEEEEEE");
+        console.log(uint32(uint64(_gateKey)));
+        console.log(uint16(uint64(_gateKey)));
         require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
         require(uint32(uint64(_gateKey)) != uint64(_gateKey), "GatekeeperOne: invalid gateThree part two");
         require(uint32(uint64(_gateKey)) == uint16(uint160(tx.origin)), "GatekeeperOne: invalid gateThree part three");
@@ -25,6 +30,7 @@ contract GatekeeperOne {
     }
 
     function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
+        console.log(gasleft());
         entrant = tx.origin;
         return true;
     }
